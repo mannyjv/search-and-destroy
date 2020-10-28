925802f80cef541fde74bef0871655fdf57bc273
Valentyna Vasylieva <smithaddaf@gmail.com>
2020-10-28 14:48:53 -0500

    binary-search done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,22 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+
+	while (array.length>1){
+		const middlePoint= Math.floor(array.length/2);
+		if (array[middlePoint]===target || array[0]===target){
+			return true
+		} else {
+			if (array[middlePoint]>target){
+				array.splice(middlePoint)
+				binarySearch(array, target)
+			} else {
+				array.splice(0,middlePoint)
+				binarySearch(array, target)
+			}
+		}
+	}
+	return false 
 };
 
 /*
