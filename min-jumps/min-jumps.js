e58f564006d0c16d041f7e34ecc83951af2faec4
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2020-10-28 23:22:46 +0100

    working on minJumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,6 +2,15 @@
 
 // Complete this algo
 const minJumps = arr => {
+  const numberOfJumpsArr = [0]
+
+  for (let i = 0; i < arr.length; i++) {
+    // let curNum = arr[i]
+    for (let j = i+1; j < i + curNum; j++) {
+      let curIndex = j
+      let curVal = array[j]
+    }
+  }
 
 };
 
